c93e55b4bb01a3490747e2e753a787aa84fd929a
Muller Quentin <71487049+quentin-muller@users.noreply.github.com>
2021-11-17 20:31:14 +0100

    rien de neuf

--- Labo05_ssh/run_all.sh
+++ Labo05_ssh/run_all.sh
@@ -2,9 +2,8 @@
 
 #make install
 
-./configure --prefix=/home/lmi/SeS/Labo05_ssh/nanopi --host=aarch64-none-linux-gnu
-
-make
+./configure --prefix=/home/lmi/SeS/Labo05_ssh/nanopi --host=aarch64-none-linux-gnu --sysconfdir=/root/sshd
 
 sed -i 's/STRIP_OPT=-s/STRIP_OPT=-s --strip-program=aarch64-none-linux-gnu-strip/g' openssh-8.8p1/Makefile
 
+make install
